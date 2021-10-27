c04f685601dcbbfba3ac1cb2ad85406214e9130f
alexaoh <alexaoh@users.noreply.github.com>
2021-10-27 12:59:15 +0200

    Made corrections and renamed Unit1Part2Task1.R

--- Unit1Part2Task1.R
+++ Unit1Part2Task1.R
@@ -4,7 +4,7 @@ p <- seq(from=0, to=1,by=0.01)
 
 power <- function(p){
   # Power function with continuous correction. 
-  pnorm((40-n*p)/(sqrt(n*p*(1-p)))-0.5)+1-pnorm((60-n*p)/(sqrt(n*p*(1-p)))+0.5)
+  pnorm((40-0.5-n*p)/(sqrt(n*p*(1-p))))+1-pnorm((60+0.5-n*p)/(sqrt(n*p*(1-p))))
 }
 
 plot(NULL, NULL, xlim = c(0,1), ylim = c(0,2), main = "Power Function", xlab = "p", ylab = "Densities")
@@ -12,3 +12,6 @@ lines(p, pnorm((40-n*p)/(sqrt(n*p*(1-p)))), lty = 1)
 lines(p, 1-pnorm((60-n*p)/(sqrt(n*p*(1-p)))), lty = 2)
 
 lines(p, power(p), lty = 1, col = 2)
+
+# Probability that was asked for in a)
+power(0.5)
